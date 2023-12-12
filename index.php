b2b2853e4f0f06761f9c31b7f9bd1647b167c9f4
CaueMondek <cauemondekk@gmail.com>
2023-12-12 20:37:21 -0300

    :tada: init: First commit!

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php  
+
+    $items = array(['imagem'=>'item1.png','nome'=>'item1','preco'=>'200'],['imagem'=>'item2.png','nome'=>'item2','preco'=>'150'],['imagem'=>'item3.png','nome'=>'item3','preco'=>'180'],);
+
+?>
